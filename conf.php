3922f2b66401ac6ee4f4f51dcffcd234d50b324b
tefanodaniel <stefanodanieltusa@gmail.com>
2023-10-03 11:38:41 -0400

    First commit

---
+++ conf.php
@@ -0,0 +1,9 @@
+
+<?php
+	//these are credentials which will be used by open.php to create connection to dbase
+	//make updates here as noted below, and save this file as conf.php
+	$dbhost = "localhost"; // database host
+	$dbuser = "root";    // username on db host: replace 21sp_jhopkin1 with yours
+	$dbpass = "jimmy";            // password on db host: replace ***** with yours
+	$dbname = "Usedcars"; // the db's name: replace 21sp_jhopkin1_db with yours
+?>
